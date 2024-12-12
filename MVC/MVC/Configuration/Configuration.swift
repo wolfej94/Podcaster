3009ab781f2b82c1abf64badee3af5478520e75f
jwolfe1994 <james.wolfe@news.co.uk>
2024-12-12 01:51:48 +0000

    Updated schemes to find secret value for podcast index

--- MVC/MVC/Configuration/Configuration.swift
+++ MVC/MVC/Configuration/Configuration.swift
@@ -9,4 +9,5 @@ import Foundation
 
 struct Configuration {
     static let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as! String
+    static let apiSecret = Bundle.main.object(forInfoDictionaryKey: "API_SECRET") as! String
 }
